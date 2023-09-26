8c15ebff0855d91c804e5f2bcedb4cee18cb0009
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-09-26 21:56:41 +0700

    Commit 5 : Fondamentaux React [Exercice 5]

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,33 +1,49 @@
 // Ajouter du Style CSS
-// http://localhost:3000/isolated/exercise/05.js
+// 🚀 Customisation avec size et color
+// http://localhost:3000/isolated/final/05.js
 
-import * as React from 'react'
-import '../05-styles.css'
+import * as React from "react";
+import "../05-styles.css";
 
-// 🐶 Met du style sur les 3 conteneurs en te basant sur leur texte.
-// 🐶 Nous avons importé le fichier CSS qui contient des classes
-// 'container' 'container-small' etc ...
-
-// 🐶 Utilise le prop `className` pour les propriétés générales du conteneur et une
-// une specificité. (className aura donc 2 classes CSS)
-// 🤖 className='container container-small' par exemple
-
-// 🐶 Utilise ensuite du CSS Inline avec le prop `style` pour gerer le background color
-// et la couleur du texte
-// 🤖 style:{{color:'white', backgroundColor:'red'}}
-
-const smallContainer = <div>Petit conteneur dark texte blanc</div>
-const mediumContainer = <div>Moyen conteneur rose texte noire</div>
-const largeContainer = <div>Grand conteneur vert rose texte noire</div>
+function Container({ className = "", children, style, color, size }) {
+  const sizeClass = size ? `container--${size}` : "";
+  return (
+    <div
+      className={`container ${sizeClass} ${className}`}
+      style={{ border: "1px solid", color, ...style }}
+    >
+      {children}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      {smallContainer}
-      {mediumContainer}
-      {largeContainer}
+      <Container
+        size="small"
+        color="white"
+        style={{ backgroundColor: "darkslateblue" }}
+      >
+        Petit conteneur dark texte blanc
+      </Container>
+      <Container
+        size="medium"
+        color="black"
+        style={{ backgroundColor: "pink" }}
+      >
+        Moyen conteneur rose texte noire
+      </Container>
+      <Container
+        size="large"
+        color="black"
+        style={{ backgroundColor: "lightgreen" }}
+      >
+        Grand conteneur vert rose texte noire
+      </Container>
+      <Container className="container--custom">Custom</Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
