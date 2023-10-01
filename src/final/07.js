0c5b66d205a4ad112f60a789f067f536a4bf603a
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-10-01 21:58:31 +0700

    Commit 7 : Fondamentaux React [Exercice 7]

--- src/final/07.js
+++ src/final/07.js
@@ -1,17 +1,24 @@
 // Rendu de tableaux
-// http://localhost:3000/isolated/final/05.js
+// 🚀 Composition et condition
+// http://localhost:3000/alone/final/06.bonus-1.js
 
 import * as React from 'react'
 
-const skills = []
-skills.push(<li key="1">HTML</li>)
-skills.push(<li key="2">CSS</li>)
-skills.push(<li key="3">JS</li>)
-skills.push(<li key="4">REACT</li>)
-skills.push(<li key="5">ANGULAR</li>)
-
+const skills = [
+  {id: 'e313', value: 'HTML'},
+  {id: 'f980', value: 'CSS'},
+  {id: '11eb', value: 'JS'},
+  {id: '9a03', value: 'REACT'},
+  {id: '65d4', value: 'ANGULAR'},
+]
 function MesSkills() {
-  return <ul>{skills.map(skill => skill)}</ul>
+  return (
+    <ul>
+      {skills.map(skill => (
+        <li key={skill.id}>{skill.value}</li>
+      ))}
+    </ul>
+  )
 }
 
 function App() {
